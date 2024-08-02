1d8017fd607d1fbe1d2c2f66c0caa9682b226e45
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-08-02 17:35:30 -0300

    CRUD operations working

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Car;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         $cars = Car::orderBy('id', 'DESC')->get();
 
@@ -16,5 +18,98 @@ class CarController extends Controller
             'status' => true,
             'cars' => $cars,
         ]);
+
+    }
+
+    public function show(Car $car): JsonResponse
+    {
+
+        return response()->json([
+            'status' => true,
+            'car' => $car,
+        ]);
+
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+
+        try {
+
+            $car = Car::create([
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'value' => $request->value,
+                'license' => $request->license,
+                'color' => $request->color
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'car' => $car,
+            ], 201);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'status' => false,
+                'message' => "erro ao adicionar",
+            ]);
+
+        }
+
+    }
+
+    public function update(Request $request, Car $car): JsonResponse
+    {
+
+        try {
+
+            $car->update([
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'value' => $request->value,
+                'license' => $request->license,
+                'color' => $request->color
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'car' => $car,
+            ], 201);
+
+        } catch (Exception $e){
+
+            return response()->json([
+                'status' => false,
+                'message' => "erro ao editar carro",
+            ]);
+
+        }
+
+    }
+
+    public function destroy(Car $car): JsonResponse
+    {
+        try {
+
+            $car->delete();
+
+            return response()->json([
+                'status' => false,
+                'message' => "apagado com sucesso",
+            ]);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+
+                'status' => false,
+                'message' => "erro ao deletar carro",
+            ]);
+
+        }
     }
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/cars', [CarController::class, 'index'])->name('index');
+
+Route::get('/cars/{car}', [CarController::class, 'show'])->name('show');
+
+Route::post('/cars', [CarController::class, 'store'])->name('store');
+
+Route::put('/cars/{car}', [CarController::class, 'update'])->name('update');
+
+Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('destroy');
